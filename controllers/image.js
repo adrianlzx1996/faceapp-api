406f1fe49af2840f81a129bd00f1d4207434dc4a
Adrian Leong <leongadrian36@gmail.com>
2019-05-19 21:03:21 +0800

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,36 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+	apiKey: "7a70044ab23c4918b09daef65eb193a6"
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+		.then(data => {
+			res.json(data);
+		})
+		.catch(err => res.status(400).json("unable to work with API"));
+};
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+
+	db("users")
+		.where("id", "=", id)
+		.increment("entries", 1)
+		.returning("entries")
+		.then(entries => {
+			res.json(entries[0]);
+		})
+		.catch(err => res.status(400).json("error updating entries"));
+
+	// if (!found) {
+	// 	res.status(400).json("error updating entries");
+	// }
+};
+
+module.exports = {
+	handleImage: handleImage,
+	handleApiCall: handleApiCall
+};
